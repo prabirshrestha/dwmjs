724617696ce5d3e51ac183fe558f741b638f14d8
Prabir <mail@prabir.me>
2019-11-12 01:31:25 -0800

    setup window and class for windows

--- dwmjs.c
+++ dwmjs.c
@@ -9,11 +9,15 @@
 #include <stdlib.h>
 #include "duktape.h"
 
+#define NAME					"dwmjs" 	/* Used for window name/class */
+static HWND dwmhwnd;
 static duk_context *duk_ctx;
-void cleanup();
-void die(const char *fmt, ...);
+static void cleanup();
+static void die(const char *fmt, ...);
+static void setup(HINSTANCE hInstance);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-void
+static void
 die(const char *fmt, ...) {
     char buf[5000];
     va_list args;
@@ -27,12 +31,55 @@ die(const char *fmt, ...) {
 	exit(EXIT_FAILURE);
 }
 
-void
+static void
 cleanup() {
     if (duk_ctx) {
         duk_destroy_heap(duk_ctx);
         duk_ctx = NULL;
     }
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+		case WM_CREATE:
+			break;
+        case WM_CLOSE:
+			cleanup();
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+static void
+setup(HINSTANCE hInstance) {
+    WNDCLASSEX winClass;
+
+	winClass.cbSize = sizeof(WNDCLASSEX);
+	winClass.style = 0;
+	winClass.lpfnWndProc = (WNDPROC)WndProc;
+	winClass.cbClsExtra = 0;
+	winClass.cbWndExtra = 0;
+	winClass.hInstance = hInstance;
+	winClass.hIcon = NULL;
+	winClass.hIconSm = NULL;
+	winClass.hCursor = NULL;
+	winClass.hbrBackground = NULL;
+	winClass.lpszMenuName = NULL;
+	winClass.lpszClassName = NAME;
+
+	if (!RegisterClassEx(&winClass))
+		die("Error registering dwmjs window class");
+
+	dwmhwnd = CreateWindowEx(0, NAME, NAME, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
+
+	if (!dwmhwnd)
+		die("Error creating dwmjs window");
 }
 
 static duk_ret_t
@@ -47,10 +94,18 @@ WINAPI WinMain(HINSTANCE hInstance,
                HINSTANCE hPrevInstance,
                LPSTR lpCmdLine,
                int nShowCmd) {
+    MSG msg;
+
     duk_context *ctx = duk_create_heap_default();
     if (!ctx) {
         die("Failed to create duktape context");
         return EXIT_FAILURE;
+    }
+
+    setup(hInstance);
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
     if (duk_safe_call(ctx, jduk_init_context, NULL, 0, 1) != 0) {
