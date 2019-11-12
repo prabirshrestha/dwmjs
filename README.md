8585bb87b74d99ceab8784e0eea29b06f45d6099
Prabir <mail@prabir.me>
2019-11-12 00:09:04 -0800

    compile on windows

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dynamic Window Manager with JavaScript configuration
 Configure windows with JavaScript.
+
+# Building
+
+## Windows
+
+Install [scoop](https://scoop.sh/), `make` and `llvm clang v9.0.0`.
+```cmd
+scoop install make llvm
+make
+```
--- config.mk
+++ config.mk
@@ -4,10 +4,10 @@ VERSION = alpha
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
-CFLAGS = -target x86_64-pc-windows-gnu -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
-LDFLAGS = -target x86_64-pc-windows-gnu -mwindows -s
-# CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
-# LDFLAGS = -target x86_64-windows-msvc
+# CFLAGS = -target x86_64-pc-windows-gnu -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
+# LDFLAGS = -target x86_64-pc-windows-gnu -mwindows -s
+CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
+LDFLAGS = -target x86_64-windows-msvc
 
 EXE=.exe
 
