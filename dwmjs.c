b8fc0756ea6d2cbc969c9d3202693dce2029e92b
Prabir <mail@prabir.me>
2019-11-12 07:14:46 -0800

    set x,y,width and height of the monitor

--- dwmjs.c
+++ dwmjs.c
@@ -112,10 +112,31 @@ jduk_get_all_monitors(duk_context *ctx) {
         DWORD monitorNum = 0;
         while (EnumDisplayDevices(display_device.DeviceName, monitorNum, &new_display_device, 0))
         {
+            // Get the display mode settings of this device.
+            DEVMODE devMode;
+            if (!EnumDisplaySettings(display_device.DeviceName, ENUM_CURRENT_SETTINGS, &devMode))
+                continue;
+
             duk_idx_t device_obj_idx = duk_push_object(ctx);
 
             duk_push_string(ctx, "id");
             duk_push_string(ctx, new_display_device.DeviceID);
+            duk_put_prop(ctx, device_obj_idx);
+
+            duk_push_string(ctx, "x");
+            duk_push_number(ctx, devMode.dmPosition.x);
+            duk_put_prop(ctx, device_obj_idx);
+
+            duk_push_string(ctx, "y");
+            duk_push_number(ctx, devMode.dmPosition.y);
+            duk_put_prop(ctx, device_obj_idx);
+
+            duk_push_string(ctx, "width");
+            duk_push_number(ctx, devMode.dmPelsWidth);
+            duk_put_prop(ctx, device_obj_idx);
+
+            duk_push_string(ctx, "height");
+            duk_push_number(ctx, devMode.dmPelsHeight);
             duk_put_prop(ctx, device_obj_idx);
 
             duk_put_prop_index(ctx, monitors_array_idx, monitor_count);
