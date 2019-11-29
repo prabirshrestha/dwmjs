aa6979bbee07d923c574cc54a4f94c969431571c
Prabir <mail@prabir.me>
2019-11-28 22:47:57 -0800

    remove CPPFLAGS from config.mk

--- config.mk
+++ config.mk
@@ -3,10 +3,9 @@ VERSION = alpha
 # Customize below to fit your system
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\"
-# CFLAGS = -target x86_64-pc-windows-gnu -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
+# CFLAGS = -target x86_64-pc-windows-gnu -std=c99 -pedantic -Wall -Os -DVERSION=\${VERSION}\"
 # LDFLAGS = -target x86_64-pc-windows-gnu -mwindows -s
-CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
+CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os -DVERSION=\"${VERSION}\"
 LDFLAGS = -target x86_64-windows-msvc
 
 ifeq ($(OS),Windows_NT)
